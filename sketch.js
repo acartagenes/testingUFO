1858f86ffde73a8d8bdd566f42ea8abafcd80b67
acartagenes <70467344+acartagenes@users.noreply.github.com>
2020-10-24 18:45:45 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,31 @@
-// Daniel Shiffman
-// http://youtube.com/thecodingtrain
-// https://thecodingtrain.com/CodingChallenges/111-animated-sprite.html
-
-// Horse Spritesheet from
-// https://opengameart.org/content/2d-platformer-art-assets-from-horse-of-spring
-
-// Animated Sprite
-// https://youtu.be/3noMeuufLZY
-
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
 let spritesheet;
 let spritedata;
 
 let animation = [];
 
-let horses = [];
+let ufos = [];
 
 function preload() {
-  spritedata = loadJSON('horse/horse.json');
-  spritesheet = loadImage('horse/Aliens clone copy.png');
+  const shipSpritesheet = loadSpriteSheet("alien/SpaceShip.png", 32, 32, 12);
+  shipAnim = loadAnimation(shipSpritesheet);
+  ship = createSprite(CANVAS_WIDTH / 2, CANVAS_HEIGHT- 50, 32, 32);
+  ship.moveSpeed = 5;
+  ship.scale = (2.0)
+  
+  spritedata = loadJSON('alien/ufo.json');
+  spritesheet = loadImage('alien/Aliens clone copy.png');
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  ship.addAnimation("move", shipAnim);
+  ship.addImage("still", loadImage("alien/ship_still.png"));
+  ship.setDefaultCollider();
+
   let frames = spritedata.frames;
   for (let i = 0; i < frames.length; i++) {
     let pos = frames[i].position;
@@ -29,17 +33,51 @@ function setup() {
     animation.push(img);
   }
 
-  for (let i = 0; i < 5; i++) {
-    horses[i] = new Sprite(animation, 0, i * 75, random(0.1, 0.4));
+  for (let i = 0; i < 3; i++) {
+    ufos[i] = new Sprite(animation, 0, i * 75, 0.1, 0.4);
   }
 }
 
+function update(object) {
+  if (keyDown("up") || keyDown("down") || keyDown("left") || keyDown("right")) {
+    if (keyDown("up")) {
+      object.addSpeed(2, 270);
+    }
+    if (keyDown("down")) {
+      object.addSpeed(2, 90);
+    }
+    if (keyDown("left")) {
+      object.addSpeed(2, 180);
+      object.mirrorX(-1);
+    }
+    if (keyDown("right")) {
+      object.addSpeed(2, 0);
+      object.mirrorX(1);
+    }
+  } else {
+    object.setSpeed(0);
+  }
+  drawObject(object);
+}
+
+function drawObject(object) {
+  if (object.getSpeed() > 0.0001) {
+    object.changeAnimation("move");
+  } else {
+    object.changeImage("still");
+  }
+  ship.limitSpeed(ship.moveSpeed);
+  drawSprite(object);
+}
+
+
 function draw() {
   background(0);
+  update(ship);
 
-  for (let horse of horses) {
-    horse.show();
-    horse.animate();
+  for (let ufo of ufos) {
+    ufo.show();
+    ufo.animate();
   }
 
   // image(animation[frameCount % animation.length], 0, 0);
